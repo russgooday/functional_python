dabcc3c5ad9ca3be1fe380ba300264bdbf792044
russgooday <rpg_digital@yahoo.co.uk>
2024-04-19 00:48:48 +0100

    Update to docstrings

--- package/internals/_prop.py
+++ package/internals/_prop.py
@@ -3,8 +3,13 @@
 
 def _pad_list_end(lst: list, targ_len:int, fill=None):
     """
-    Pad the list to the target length with fill value.
-    @returns: a shallow clone
+        Pad the list to the target length with a fill value.
+
+        e.g.
+            _pad_list_end([1, 2], 3) → [1, 2, None, None]
+            _pad_list_end([1, 2, 3], 2) → [1, 2, 3]
+
+        @returns: a shallow clone
     """
     padding = max(0, targ_len + 1 - len(lst))
     return  [*lst, *[fill] * padding]
@@ -14,11 +19,11 @@ def _get_prop(key:str|int, obj:dict|list, default=None):
     '''
         tries to get property from dict or lists
 
-        :param obj: the object to search e.g. dict, list
-        :param key: the key to search for
-        :param default: the value to return if key is not found
+        @param obj: the object to search e.g. dict, list
+        @param key: the key to search for
+        @param default: the value to return if key is not found
 
-        :return: value or default
+        @return: value or default
     '''
     try:
         return obj[key]
@@ -27,6 +32,15 @@ def _get_prop(key:str|int, obj:dict|list, default=None):
 
 
 def _set_prop(key:str|int, val:any, obj:dict|list):
+    '''
+        sets property
+
+        @param key: the key to set
+        @param value: the value to set
+        @param obj: the object to be modified
+
+        @return: modfied clone of object
+    '''
     if isinstance(key, int) and isinstance(obj, list):
         lst = _pad_list_end(obj, key)
         lst[key] = val
@@ -38,4 +52,13 @@ def _set_prop(key:str|int, val:any, obj:dict|list):
 
 
 def _prop_equals(val, key:str|int, obj:dict|list):
+    '''
+        compares property to given value
+
+        @param value: the value to set
+        @param key: the key to set
+        @param obj: the object to be modified
+
+        @return: bool
+    '''
     return _get_prop(key, obj, False) == val
