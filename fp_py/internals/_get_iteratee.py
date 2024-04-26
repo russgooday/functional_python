5c7a71720fa6c35c871452f4ac2df931465b4a45
russgooday <rpg_digital@yahoo.co.uk>
2024-04-26 01:52:17 +0100

    updated to use where_equals and wrapping lambda removed

--- fp_py/internals/_get_iteratee.py
+++ fp_py/internals/_get_iteratee.py
@@ -1,19 +1,11 @@
 '''
     get_iteratee creates predicate or mapping functions
 '''
-
 import re
+
 from fp_py.internals._parameters import _adjust_params_count
-from fp_py.internals._prop import _get_prop, _prop_equals
-
-def _all_props_match(props, target_obj):
-    '''
-        Returns True if all properties correspond in both objects
-    '''
-    if isinstance(props, dict) and isinstance(target_obj, dict):
-        return all(_prop_equals(v, k, target_obj) for k, v in props.items())
-
-    return False
+from fp_py.prop import get_prop, prop_equals
+from fp_py.where import where_equals
 
 
 def _get_iteratee(value):
@@ -32,15 +24,15 @@ def _get_iteratee(value):
 
     if isinstance(value, (list, tuple)) and len(value) == 2:
         # [ 'key', value ]: returns True if key and value are found in object
-        return lambda obj: _prop_equals(value[1], value[0], obj)
+        return prop_equals(value[1], value[0])
 
     if isinstance(value, dict):
         # { 'key1': value1, ... }: returns True if properties correspond in both objects
-        return lambda obj: _all_props_match(value, obj)
+        return where_equals(value)
 
     if isinstance(value, str):
         # 'key': returns value of object['key'] or None
-        return lambda obj: _get_prop(value, obj)
+        return get_prop(value)
 
     if isinstance(value, re.Pattern):
         # regex: returns true if a string match is found
